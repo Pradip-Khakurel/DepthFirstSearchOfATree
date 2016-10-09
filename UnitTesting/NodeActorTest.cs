5b8e5903dfd582421cd3d51b6bb1c8c5e5d7c781
Pradip Khakurel <pradip.khakurel@gmail.com>
2016-10-09 22:41:08 +0200

    add NodeActorTest

--- UnitTesting/NodeActorTest.cs
+++ UnitTesting/NodeActorTest.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Akka.Actor;
+using Akka.TestKit.Xunit2;
+using DepthFirstSearchOfATree.AkkaDotNetExample;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,22 +10,129 @@ using Xunit;
 
 namespace DepthFirstSearchOfATree.UnitTesting
 {
-    public class NodeActorTest
+    public class NodeActorTest : TestKit
     {
-        //[Fact]
-        //public void NodeActor_should_send_AddRequest_to_its_children_when_the_message_is_not_intended_to_him()
-        //{
+        [Fact]
+        public void NodeActor_should_send_AddResult_to_the_TreeActor_when_the_message_is_for_him()
+        {
+            var nodeActor = Sys.ActorOf(Props.Create(() => new NodeActor("node")));
+            var childFactory = new TestProbeFactory("child");
 
-        //}
+            // adding child
+            nodeActor.Tell(new AddRequest(childFactory, "node", this.TestActor)); // TestActor is TreeActor
 
-        // NodeActor_should_not_send_AddRequest_to_any_child_when_the_message_is_not_intended_to_him
+            var child = childFactory.Probe;
 
-        // NodeActor_should_always_send_VisitRequest_to_its_children
+            this.ExpectMsg<AddResult>((m, s) => s == nodeActor);
+        }
 
-        // NodeActor_should_send_VisitRequest_to_a_newly_created_child
+        [Fact]
+        public void NodeActor_should_not_send_AddResult_to_the_TreeActor_when_the_message_is_not_for_him()
+        {
+            var nodeActor = Sys.ActorOf(Props.Create(() => new NodeActor("node")));
+            var childFactory = new TestProbeFactory("child");
 
-        // NodeActor_not_should_send_VisitRequest_to_the_second_child_without_receiving_VisitResult_from_the_first_one
+            // adding to an other node
+            nodeActor.Tell(new AddRequest(childFactory, "otherNode", this.TestActor));
 
-        // NodeActor_not_should_send_VisitRequest_to_its_parent_whithout_receiving_VisitRequest_from_its_last_child
+            this.ExpectNoMsg();
+        }
+
+        [Fact]
+        public void NodeActor_should_always_send_VisitRequest_to_its_child()
+        {
+            var nodeActor = Sys.ActorOf(Props.Create(() => new NodeActor("node")));
+            var child1Factory = new TestProbeFactory("child1");
+
+            // adding child1
+            nodeActor.Tell(new AddRequest(child1Factory, "node", this.TestActor));
+            this.ExpectMsg<AddResult>();
+
+            nodeActor.Tell(new VisitRequest(this.TestActor, nodeActor, null));
+
+            var child1 = child1Factory.Probe;
+
+            child1.ExpectMsg<VisitRequest>();
+        }
+
+
+        [Fact]
+        public void NodeActor_not_should_send_VisitRequest_to_its_second_child_without_receiving_VisitResult_from_the_first_one()
+        {
+            var nodeActor = Sys.ActorOf(Props.Create(() => new NodeActor("node")));
+            var child1Factory = new BlackHoleActorFactory("child1");
+            var child2Factory = new TestProbeFactory("child2");
+
+            // adding child1
+            nodeActor.Tell(new AddRequest(child1Factory, "node", this.TestActor));
+
+            // adding child2
+            nodeActor.Tell(new AddRequest(child2Factory, "node", this.TestActor));
+
+            nodeActor.Tell(new VisitRequest(this.TestActor, nodeActor, null));
+
+            var child2 = child2Factory.Probe;
+
+            child2.ExpectNoMsg();
+        }
+
+        [Fact]
+        public void NodeActor_should_send_VisitRequest_to_the_second_child_when_receiving_VisitResult_from_the_first_one()
+        {
+            var nodeActor = Sys.ActorOf(Props.Create(() => new NodeActor("node")));
+            var child1Factory = new TestProbeFactory("child1");
+            var child2Factory = new TestProbeFactory("child2");
+
+            // adding child1
+            nodeActor.Tell(new AddRequest(child1Factory, "node", this.TestActor));
+            this.ExpectMsg<AddResult>();
+
+            // adding child2
+            nodeActor.Tell(new AddRequest(child2Factory, "node", this.TestActor));
+            this.ExpectMsg<AddResult>();
+
+            nodeActor.Tell(new VisitRequest(this.TestActor, nodeActor, null));
+
+            var child1 = child1Factory.Probe;
+            var child2 = child2Factory.Probe;
+
+            child1.ExpectMsg<VisitRequest>(m => {
+                m.Sender.Tell(new VisitResult(m)); // sending VisitResult after receiving VisitRequest
+            });
+
+            child2.ExpectMsg<VisitRequest>();
+        }
+
+        [Fact]
+        public void NodeActor_should_send_VisitResult_to_its_parent_after_receiving_VisitRequest_from_its_last_child()
+        {
+            var nodeActor = Sys.ActorOf(Props.Create(() => new NodeActor("node")));
+            var child1Factory = new TestProbeFactory("child1");
+            var child2Factory = new TestProbeFactory("child2");
+
+            // adding child1 - TestActor is the parent of NodeActor
+            nodeActor.Tell(new AddRequest(child1Factory, "node", this.TestActor));
+            
+            // adding child2
+            nodeActor.Tell(new AddRequest(child2Factory, "node", this.TestActor));
+
+            this.ExpectMsg<AddResult>(); // first AddRequest
+            this.ExpectMsg<AddResult>(); // second AddRequest
+
+            nodeActor.Tell(new VisitRequest(this.TestActor, nodeActor, null));
+
+            var child1 = child1Factory.Probe;
+            var child2 = child2Factory.Probe;
+
+            child1.ExpectMsg<VisitRequest>(m1 => { // child1 receives the VisitRequest first ...
+                m1.Sender.Tell(new VisitResult(m1));
+            });
+
+            child2.ExpectMsg<VisitRequest>(m2 => { // then child2 receives the VisitRequest ...
+                m2.Sender.Tell(new VisitResult(m2));  
+            });
+
+            this.ExpectMsg<VisitResult>(); // and then NodeActor sends back VisitResult
+        }
     }
 }
